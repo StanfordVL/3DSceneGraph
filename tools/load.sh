83111c6d63e09b9ade85b62a2cff052ac5b954d0
ir0 <ir0@users.noreply.github.com>
2020-01-13 16:18:40 -0800

    Updated load.sh with medium split model list

--- tools/load.sh
+++ tools/load.sh
@@ -8,10 +8,12 @@ gibson_mesh_path=/path/to/Gibson/database       # system path to Gibson database
 export_viz_path=$project_path/visuals           # system path to export wavefront files (if visualize is set to 1)
 
 # list of models in the tiny Gibson split
-models='Allensville Beechwood Benevolence Coffeen Collierville Corozal Cosmos Darden Forkland Hanson Hiteman Ihlen Klickitat Lakeville Leonardo Lindenwood Markleeville Marstons McDade Merom Mifflinburg Muleshoe Newfields Noxapater Onaga Pinesdale Pomaria Ranchester Shelbyville Stockman Tolstoy Uvalda Wainscott Wiconisco Woodbine'
+tiny_models='Allensville Beechwood Benevolence Coffeen Collierville Corozal Cosmos Darden Forkland Hanson Hiteman Ihlen Klickitat Lakeville Leonardo Lindenwood Markleeville Marstons McDade Merom Mifflinburg Muleshoe Newfields Noxapater Onaga Pinesdale Pomaria Ranchester Shelbyville Stockman Tolstoy Uvalda Wainscott Wiconisco Woodbine'
+# list of models in the medium Gibson split
+medium_models='Adairsville Airport Albertville Anaheim Ancor Andover Annona Arkansaw Athens Bautista Bohemia Bonesteel Bonnie Brinnon Broseley Brown Browntown Byers Castor Cauthron Chilhowie Churchton Clairton Cochranton Cottonport Couderay Cousins Darrtown Donaldson Duarte Eagan Edson Emmaus Frankfort German Globe Goffs Goodfield Goodwine Goodyear Gravelly Hainesburg Helton Highspire Hildebran Hillsdale Hominy Hordville Hortense Irvine Kemblesville Kobuk Losantville Lynchburg Maida Marland Martinville Maugansville Micanopy Musicks Natural Neibert Neshkoro Newcomb Nuevo Oyens Pablo Pamelia Parole Pearce Pittsburg Poipu Potterville Readsboro Rockport Rogue Rosser Sands Scioto Shelbiana Silas Soldier Southfield Springerville Stilwell Sugarville Sunshine Sussex Sweatman Swisshome Swormville Thrall Tilghmanton Timberon Tokeland Touhy Tyler Victorville Waipahu Wando Westfield Willow Wilseyville Winooski Wyldwood'
 
-#iterate over models
-for model in $models; do
+#iterate over models. Choose: tiny_models for tiny split or medium_models for medium split
+for model in $tiny_models; do
     python $file_path --model $model \
                     --verified $verified \
                     --visualize $visualize \
